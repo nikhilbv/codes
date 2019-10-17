26b33fbd88a207b4afba3a3325876b38bb577911
nikhilbv <nikhil.bangale@gmail.com>
2019-10-17 17:50:12 +0530

    new changes updated

--- python/no_of_lanes.py
+++ python/no_of_lanes.py
@@ -2,24 +2,32 @@ import json
 import os
 import cv2
 
-root_dir = '/home/nikhil/Documents/annotations/'
-json_file = 'ann.json'
-# root_dir = '/aimldl-dat/data-public/tusimple-sub/train_set/'
-#json_file = 'label_data_0313.json'
+# root_dir = '/home/nikhil/Documents/annotations/'
+# json_file = 'ann.json'
+root_dir = '/aimldl-dat/data-public/tusimple/train_set/'
+# root_dir = '/aimldl-dat/data-public/tusimple/'
+# json_file = 'label_data_0313.json'
+json_file = 'label_data_0531.json'
 # json_file = 'label_data_0601.json'
-print(root_dir)
-print(json_file)
+# json_file = 'test_label.json'
+# json_file = 'test_baseline.json'
+# print(root_dir)
+# print(json_file)
 
 with open(root_dir + json_file, 'r') as file:
   json_lines = file.readlines()
-  res_lanes = {'0_lanes':0,'1_lanes':0,'2_lanes':0,'3_lanes':0,'4_lanes':0}
+  res_lanes = {'0_lanes':0,'1_lanes':0,'2_lanes':0,'3_lanes':0,'4_lanes':0,'5_lanes':0,'6_lanes':0}
   for line_index,val in enumerate(json_lines):
-    #print(line_index)
+    # print(line_index)
     json_line = json_lines[line_index]
     sample = json.loads(json_line)
+    # print("sample :{}".format(sample))
     lanes = sample['lanes']
+    # print("lanes :{}".format(lanes))
+    # image = sample['raw_file']
+
     res_lane = []
-    #print(len(lanes))
+  #   #print(len(lanes))
     for lane in lanes:
       lane_id_found=False
       for lane_id in lane:
@@ -34,11 +42,18 @@ with open(root_dir + json_file, 'r') as file:
     if len(res_lane) == 0:
       res_lanes['0_lanes']=res_lanes['0_lanes']+1
     elif len(res_lane) == 1:
+      # print(json_line)
       res_lanes['1_lanes']=res_lanes['1_lanes']+1
     elif len(res_lane) == 2:
+      # print(json_line)
       res_lanes['2_lanes']=res_lanes['2_lanes']+1
     elif len(res_lane) == 3:
+      print(json_line)
       res_lanes['3_lanes']=res_lanes['3_lanes']+1
     elif len(res_lane) == 4:
       res_lanes['4_lanes']=res_lanes['4_lanes']+1
+    elif len(res_lane) == 4:
+      res_lanes['5_lanes']=res_lanes['5_lanes']+1
+    elif len(res_lane) == 4:
+      res_lanes['6_lanes']=res_lanes['6_lanes']+1
   print(res_lanes)
--- python/resize.py
+++ python/resize.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python
 from PIL import Image
 import os, sys
+import shutil
+import tqdm
 
+# from_path = "/aimldl-dat/data-gaze/AIML_Database/lnd-011019_165046/images/"
+# from_path = "/aimldl-dat/data-gaze/AIML_Database/lnd-101019_103444/images/"
+from_path = "/aimldl-dat/data-gaze/AIML_Database/lnd-151019_104101/images/"
+print("from_path : {}".format(from_path))
+dirs = os.listdir( from_path )
+to_path = os.path.join(from_path,'resized')
+print("to path : {}".format(to_path))
 
-# path = "/home/nikhil/Documents/advanced-lane-detection/test-img/"
-path = "/home/nikhil/Documents/annotations/images/"
-path = "/home/nikhil/Documents/images/"
-dirs = os.listdir( path )
+if os.path.exists(to_path):
+    shutil.rmtree(to_path)
+os.makedirs(to_path)
 
-print(dirs)
+# print(dirs)
 
 def resize():
-    for item in dirs:
-        print("image:{}".format(path+item))
-        if os.path.isfile(path+item):
-            im = Image.open(path+item)
+    # for item in dirs:
+    for idx,item in tqdm.tqdm(enumerate(dirs), total = len(dirs)):
+        # print("image:{}".format(from_path+item))
+        if os.path.isfile(from_path+item):
+            im = Image.open(from_path+item)
             # print("im:{}".format(im))
-            f, e = os.path.splitext(path+item)
+            f, e = os.path.splitext(from_path+item)
             # print("f:{}".format(f))
             # print("e:{}".format(e))
             imResize = im.resize((1280,720), Image.ANTIALIAS)
-            imResize.save(f + ' resized.jpg', 'JPEG', quality=100)
+            imResize.save(to_path+'/'+item, 'JPEG', quality=100)
 
 resize()
