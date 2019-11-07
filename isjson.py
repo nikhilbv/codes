9bb188b33d95bc497b0263da9ba6fa00e10118f4
nikhilbv <nikhil.bangale@gmail.com>
2019-11-07 18:48:08 +0530

    repo cleanup

--- isjson.py
+++
@@ -1,9 +0,0 @@
-def isjson(src):
-  file = src.split('/')[-1].split('.')[-1]
-  if file == 'json':
-    return file
-
-src = '/aimldl-dat/data-gaze/AIML_Database/lnd-211019_120637/images-p1-230919_AT1_via205_081019_tuSimple-211019_120637.json'
-
-if isjson(src):
-  print("true") 
--- tests/hack3.py
+++
@@ -1,5 +0,0 @@
-Sentence = "GeeksforGeeks is good to learn"
-words = Sentence.split(" ") 
-newWords = [word[::-1] for word in words] 
-newSentence = " ".join(newWords) 
-print(newSentence)
